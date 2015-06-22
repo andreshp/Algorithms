121a13af6e0d55c704b892db5a4918e65741301a
andreshp <andreshp9@gmail.com>
2015-06-22 20:10:36 +0200

    Solves problem 5 of EpicCode

---
+++ Problems/Hackerrank/EpicCode/5-1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+###################################################################
+# Author: andreshp
+# Email:  andreshp9@gmail.com
+# Date:   2015-06-22
+# Last Modified by:   andreshp
+# Last Modified time: 2015-06-22
+# File Name: 5-1.py
+###################################################################
+
+import math
+import sys
+import time
+
+#---------------------------- FUNCTIONS ----------------------------#
+
+def func1:
+    pass
+
+#------------------------------ MAIN -------------------------------#
+
+N, K = [int(x) for x in input().split()]
--- Problems/Hackerrank/EpicCode/5.py
+++ Problems/Hackerrank/EpicCode/5.py
@@ -5,7 +5,7 @@
 # Email:  andreshp9@gmail.com
 # Date:   2015-06-20
 # Last Modified by:   andreshp
-# Last Modified time: 2015-06-21
+# Last Modified time: 2015-06-22
 # File Name: 5.py
 ###################################################################
 
@@ -13,49 +13,35 @@ from bisect import *
 
 #---------------------------- FUNCTIONS ----------------------------#
 
-# Recursive function
-# If there are more than 1 element sorts intervals[start:end].
-# Find the intervals from intervals[middle:end] nested in a interval of intervals[start:middle]
-# Call recursively to both parts
-def _nestedIntervals(intervals, start, end, nested):
-    if start+1 < end:
-        # Sort and find middle point
-        intervals[start:end] = sorted(intervals[start:end], key = lambda x: (x[0], -x[1]))
-        middle = (start + end) // 2
-
-        # Sort second part according to second value
-        intervals[middle:end] = sorted(intervals[middle:end],key = lambda x: x[1])
-        B2 = list(map(lambda x: x[1], intervals[middle:end]))
-        B2.sort()
-
-        #print(intervals, start, middle, end, B2)
-        # For each element in first part add intervals in second part contained
-        for i in range(start, middle):
-            nested[intervals[i]] += bisect_right(B2, intervals[i][1]) # binary search to position most to the right
-
-        #print(nested)
-        _nestedIntervals(intervals,start,middle,nested)
-        _nestedIntervals(intervals,middle,end,nested)
-
-# Compute for each interval the number of intervals nested in him.
-def nestedIntervals(intervals):
-    nested = {interval : 0 for interval in intervals}
-    _nestedIntervals(intervals, 0, len(intervals), nested)
-    return nested
+def longestIncreasingSubsequenceLength(A):
+    S = [A[0]]
+    S_index = [0]
+    for i in range(1,len(A)):
+        if A[i] > S[-1]:
+            S.append(A[i])
+            S_index.append(i)
+        else:
+            pos = bisect_left(S,A[i])
+            if pos < len(S):
+                S[pos] = A[i]
+                S_index[pos] = i
+    return len(S)
 
 #------------------------------ MAIN -------------------------------#
 
-# Read the pairs with not repeated elements:
+# Read the pairs with not repeated elements (we do not care about them):
 N = int(input())
 pairs = set()
 for i in range(0,N):
     pairs.add(input())
-pairs = list(map(lambda x: tuple(map(int, x.split())), list(pairs)))
+pairs = list(map(lambda x: tuple(map(int, x.split())), pairs))
 
-sol = 0
-nested = nestedIntervals(pairs)
-for key in nested:
-    if nested[key] == 0:
-        sol += 1
+# Sort the pairs and get the second element for every pair.
+# Now if i < j and B[i] < B[j] then pairs[i] and pairs[j] are good.
+# Note that if pair[i][0] = pair[j][0] then B[i] >= B[j].
+# Consequently, the longest pair algorithm will not take both pairs i and j
+# at the same time.
+pairs.sort(key = lambda x: (x[0], -x[1]))
+B = list(map(lambda x: x[1], pairs))
 
-print(sol)
+print(longestIncreasingSubsequenceLength(B))
